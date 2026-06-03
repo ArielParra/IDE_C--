360cbd8d20042ef533dc6d7a934be8128e514fb3
ArielParra <arielemilio_parra@hotmail.com>
2026-06-03 12:15:11 -0600

    feat: register language and style resources for asset management and update search paths to use resource URIs

--- src/ui/language_style.rs
+++ src/ui/language_style.rs
@@ -20,20 +20,15 @@ impl LanguageStyle {
         Option<sourceview5::Language>,
         Option<sourceview5::StyleScheme>,
     ) {
-        let base = std::env::current_dir().unwrap();
-
-        let lang_path = base.join("src/resources/language-specs");
-        let style_path = base.join("src/resources/styles");
-
         let mut lang_paths = self.language_manager.search_path();
-        lang_paths.push(lang_path.to_string_lossy().to_string().into());
+        lang_paths.push("resource:///com/ide_cmm/ide/resources/language-specs".into());
         let lang_paths: Vec<&str> = lang_paths.iter().map(|s| s.as_str()).collect();
         self.language_manager.set_search_path(&lang_paths);
 
         let language = self.language_manager.language("cmm");
 
         let mut style_paths = self.style_manager.search_path();
-        style_paths.push(style_path.to_string_lossy().to_string().into());
+        style_paths.push("resource:///com/ide_cmm/ide/resources/styles".into());
         let style_paths: Vec<&str> = style_paths.iter().map(|s| s.as_str()).collect();
         self.style_manager.set_search_path(&style_paths);
 
